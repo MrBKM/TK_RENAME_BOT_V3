33280e207de3598bb6ee8dc7cbbbacbdf3d872d7
BKM TG <119480777+MrBKM@users.noreply.github.com>
2022-12-07 09:56:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID =
-API_HASH =
-TOKEN =
-DB_NAME =
-DB_URL =
-CHANNEL =
-ADMIN =
+API_ID = "12694006"
+API_HASH = "8719d11809492f836004a39b42599215"
+TOKEN = "5791998848:AAFx78tuc3mhWIB2vninmzibD4dn9gc_3Wk"
+DB_NAME = "Pushpa"
+DB_URL = "mongodb+srv://Pushpa:Pushpa@cluster0.e0trkss.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = ""
+ADMIN = "953377581"
 
