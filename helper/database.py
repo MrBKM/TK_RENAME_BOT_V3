fbeeaf21a32c2de66dc0501738820c3144dbd5c2
BKM TG <119480777+MrBKM@users.noreply.github.com>
2022-12-09 07:50:39 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,8 +1,8 @@
 import pymongo 
 import os
 from helper.date import add_date
-DB_NAME = os.environ.get("DB_NAME","")
-DB_URL = os.environ.get("DB_URL","")
+DB_NAME = os.environ.get("DB_NAME","Pushpa")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://Pushpa:Pushpa@cluster0.e0trkss.mongodb.net/?retryWrites=true&w=majority")
 mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["user"]
